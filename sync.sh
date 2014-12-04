e345641fbb669ac7184061d7aec64950c640d286
Nic Young <nryoung@gmail.com>
2014-12-03 18:44:04 -0700

    Add redshift config file.

--- sync.sh
+++ sync.sh
@@ -2,3 +2,4 @@ ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/vim ~/.vim
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/bash_aliases ~/.bash_aliases
+ln -s ~/dotfiles/redshift.conf ~/.config/redshift.conf
