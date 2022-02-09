027186513a0a6ad4dcbb3cc558a30fda6349afe6
Nic Young <nryoung@gmail.com>
2022-02-08 22:00:20 -0700

    feat: switch fully to neovim and lua (#2)

---
+++ nvim/lua/keymappings.lua
@@ -0,0 +1,79 @@
+-- Map leader to comma
+vim.g.mapleader = ','
+
+-- Disable Ex mode
+vim.api.nvim_set_keymap('n', 'Q', '<Nop>', { noremap = true })
+
+-- Make Y and D yank until EOL like other capital letters
+vim.api.nvim_set_keymap('', 'Y', 'y$', {})
+vim.api.nvim_set_keymap('', 'D', 'd$', {})
+
+-- Do not move the cursoer when highlighting
+vim.api.nvim_set_keymap('n', '*', '*N', { noremap = true })
+vim.api.nvim_set_keymap('n', '#', '#N', { noremap = true })
+
+-- Clear the search highlight with <leader> n
+vim.api.nvim_set_keymap('n', '<leader>n', ':noh<CR>', { noremap = false })
+
+-- Paste mode toggle
+vim.api.nvim_set_keymap('n', '<silent> <leader>p', ':set paste!', { noremap = false })
+
+-- Softwrap text
+vim.api.nvim_set_keymap('n', '<silent> <leader>w', ':set wrap! linebreak! textwidth=0<CR>', { noremap = false })
+
+-- Sorting in visual mode
+vim.api.nvim_set_keymap('v', '<leader>s', ':sort<CR>', {})
+
+-- Opens an edit command with the path of the currently edited file filled in
+-- Normal mode: <leader>e
+vim.api.nvim_set_keymap('', '<leader>e', ':e <C-R>=expand("%:p:h") . "/" <CR>', {})
+
+-- Opens a split command with the path of the currently edited file filled in
+-- Normal mode: <leader>se
+vim.api.nvim_set_keymap('', '<leader>se', ':split <C-R>=expand("%:p:h") . "/" <CR>', {})
+
+-- Opens a tab edit command with the path of the currently edited file filled in
+-- Normal mode: <leader>ve
+vim.api.nvim_set_keymap('', '<leader>ve', ':vsplit <C-R>=expand("%:p:h") . "/" <CR>', {})
+
+-- After shifting a visual block, reselect it to be able to shift again
+vim.api.nvim_set_keymap('v', '>', '>gv', { noremap = true })
+vim.api.nvim_set_keymap('v', '<', '<gv', { noremap = true })
+
+-- Disable help
+vim.api.nvim_set_keymap('i', '<F1>', '<ESC>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<F1>', '<ESC>', { noremap = true })
+vim.api.nvim_set_keymap('v', '<F1>', '<ESC>', { noremap = true })
+
+-- Remap C-c to Esc
+vim.api.nvim_set_keymap('i', '<C-c>', '<ESC>', { noremap = true })
+
+-- Reload vim rc file
+vim.api.nvim_set_keymap('n', '<leader>rl', ':so $MYVIMRC<CR>', { noremap = true })
+
+-- Map undotree to ,u
+-- nnoremap <leader>u :UndotreeToggle<cr>
+vim.api.nvim_set_keymap('n', '<leader>u', ':UndotreeToggle<CR>', { noremap = true })
+
+-- Map various Ranger commands
+vim.api.nvim_set_keymap('n', '<leader>r', ':RangerEdit<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>rv', ':RangerVSplit<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>rs', ':RangerSplit<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>rt', ':RangerTab<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>ri', ':RangerInsert<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>ra', ':RangerAppend<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>rc', ':set operatorfunc=RangerChangeOperator<CR>g@', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>rd', ':RangerCD<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>rld', ':RangerLCD<CR>', { noremap = true })
+
+-- Map various Telescope commands
+vim.api.nvim_set_keymap('n', '<leader>f', '<cmd>Telescope find_files<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>g', '<cmd>Telescope live_grep<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>b', '<cmd>Telescope buffers<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>h', '<cmd>Telescope help_tags<CR>', { noremap = true })
+
+-- Map various Compe commands
+vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
---
+++ nvim/plugin/packer_compiled.lua
@@ -0,0 +1,221 @@
+-- Automatically generated packer.nvim plugin loader code
+
+if vim.api.nvim_call_function('has', {'nvim-0.5'}) ~= 1 then
+  vim.api.nvim_command('echohl WarningMsg | echom "Invalid Neovim version for packer.nvim! | echohl None"')
+  return
+end
+
+vim.api.nvim_command('packadd packer.nvim')
+
+local no_errors, error_msg = pcall(function()
+
+  local time
+  local profile_info
+  local should_profile = false
+  if should_profile then
+    local hrtime = vim.loop.hrtime
+    profile_info = {}
+    time = function(chunk, start)
+      if start then
+        profile_info[chunk] = hrtime()
+      else
+        profile_info[chunk] = (hrtime() - profile_info[chunk]) / 1e6
+      end
+    end
+  else
+    time = function(chunk, start) end
+  end
+  
+local function save_profiles(threshold)
+  local sorted_times = {}
+  for chunk_name, time_taken in pairs(profile_info) do
+    sorted_times[#sorted_times + 1] = {chunk_name, time_taken}
+  end
+  table.sort(sorted_times, function(a, b) return a[2] > b[2] end)
+  local results = {}
+  for i, elem in ipairs(sorted_times) do
+    if not threshold or threshold and elem[2] > threshold then
+      results[i] = elem[1] .. ' took ' .. elem[2] .. 'ms'
+    end
+  end
+
+  _G._packer = _G._packer or {}
+  _G._packer.profile_output = results
+end
+
+time([[Luarocks path setup]], true)
+local package_path_str = "/Users/nic.young/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/Users/nic.young/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/Users/nic.young/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/Users/nic.young/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/Users/nic.young/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
+if not string.find(package.path, package_path_str, 1, true) then
+  package.path = package.path .. ';' .. package_path_str
+end
+
+if not string.find(package.cpath, install_cpath_pattern, 1, true) then
+  package.cpath = package.cpath .. ';' .. install_cpath_pattern
+end
+
+time([[Luarocks path setup]], false)
+time([[try_loadstring definition]], true)
+local function try_loadstring(s, component, name)
+  local success, result = pcall(loadstring(s), name, _G.packer_plugins[name])
+  if not success then
+    vim.schedule(function()
+      vim.api.nvim_notify('packer.nvim: Error running ' .. component .. ' for ' .. name .. ': ' .. result, vim.log.levels.ERROR, {})
+    end)
+  end
+  return result
+end
+
+time([[try_loadstring definition]], false)
+time([[Defining packer_plugins]], true)
+_G.packer_plugins = {
+  QFEnter = {
+    loaded = true,
+    path = "/Users/nic.young/.local/share/nvim/site/pack/packer/start/QFEnter",
+    url = "https://github.com/yssl/QFEnter"
+  },
+  delimitMate = {
+    loaded = true,
+    path = "/Users/nic.young/.local/share/nvim/site/pack/packer/start/delimitMate",
+    url = "https://github.com/Raimondi/delimitMate"
+  },
+  ["editorconfig-vim"] = {
+    loaded = true,
+    path = "/Users/nic.young/.local/share/nvim/site/pack/packer/start/editorconfig-vim",
+    url = "https://github.com/editorconfig/editorconfig-vim"
+  },
+  falcon = {
+    loaded = true,
+    path = "/Users/nic.young/.local/share/nvim/site/pack/packer/start/falcon",
+    url = "https://github.com/fenetikm/falcon"
+  },
+  ["nvim-compe"] = {
+    loaded = true,
+    path = "/Users/nic.young/.local/share/nvim/site/pack/packer/start/nvim-compe",
+    url = "https://github.com/hrsh7th/nvim-compe"
+  },
+  ["nvim-lsp-installer"] = {
+    loaded = true,
+    path = "/Users/nic.young/.local/share/nvim/site/pack/packer/start/nvim-lsp-installer",
+    url = "https://github.com/williamboman/nvim-lsp-installer"
+  },
+  ["nvim-lspconfig"] = {
+    loaded = true,
+    path = "/Users/nic.young/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    url = "https://github.com/neovim/nvim-lspconfig"
+  },
+  ["nvim-treesitter"] = {
+    loaded = true,
+    path = "/Users/nic.young/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    url = "https://github.com/nvim-treesitter/nvim-treesitter"
+  },
+  ["nvim-treesitter-context"] = {
+    loaded = true,
+    path = "/Users/nic.young/.local/share/nvim/site/pack/packer/start/nvim-treesitter-context",
+    url = "https://github.com/romgrk/nvim-treesitter-context"
+  },
+  ["nvim-treesitter-refactor"] = {
+    loaded = true,
+    path = "/Users/nic.young/.local/share/nvim/site/pack/packer/start/nvim-treesitter-refactor",
+    url = "https://github.com/nvim-treesitter/nvim-treesitter-refactor"
+  },
+  ["nvim-ts-context-commentstring"] = {
+    loaded = true,
+    path = "/Users/nic.young/.local/share/nvim/site/pack/packer/start/nvim-ts-context-commentstring",
+    url = "https://github.com/JoosepAlviste/nvim-ts-context-commentstring"
+  },
+  ["nvim-ts-rainbow"] = {
+    loaded = true,
+    path = "/Users/nic.young/.local/share/nvim/site/pack/packer/start/nvim-ts-rainbow",
+    url = "https://github.com/p00f/nvim-ts-rainbow"
+  },
+  ["packer.nvim"] = {
+    loaded = true,
+    path = "/Users/nic.young/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    url = "https://github.com/wbthomason/packer.nvim"
+  },
+  ["plenary.nvim"] = {
+    loaded = true,
+    path = "/Users/nic.young/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    url = "https://github.com/nvim-lua/plenary.nvim"
+  },
+  ["popup.nvim"] = {
+    loaded = true,
+    path = "/Users/nic.young/.local/share/nvim/site/pack/packer/start/popup.nvim",
+    url = "https://github.com/nvim-lua/popup.nvim"
+  },
+  ["ranger.vim"] = {
+    loaded = true,
+    path = "/Users/nic.young/.local/share/nvim/site/pack/packer/start/ranger.vim",
+    url = "https://github.com/rafaqz/ranger.vim"
+  },
+  ["rust.vim"] = {
+    loaded = true,
+    path = "/Users/nic.young/.local/share/nvim/site/pack/packer/start/rust.vim",
+    url = "https://github.com/rust-lang/rust.vim"
+  },
+  ["telescope.nvim"] = {
+    loaded = true,
+    path = "/Users/nic.young/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    url = "https://github.com/nvim-telescope/telescope.nvim"
+  },
+  undotree = {
+    loaded = true,
+    path = "/Users/nic.young/.local/share/nvim/site/pack/packer/start/undotree",
+    url = "https://github.com/mbbill/undotree"
+  },
+  ["vim-airline"] = {
+    loaded = true,
+    path = "/Users/nic.young/.local/share/nvim/site/pack/packer/start/vim-airline",
+    url = "https://github.com/vim-airline/vim-airline"
+  },
+  ["vim-commentary"] = {
+    loaded = true,
+    path = "/Users/nic.young/.local/share/nvim/site/pack/packer/start/vim-commentary",
+    url = "https://github.com/tpope/vim-commentary"
+  },
+  ["vim-fugitive"] = {
+    loaded = true,
+    path = "/Users/nic.young/.local/share/nvim/site/pack/packer/start/vim-fugitive",
+    url = "https://github.com/tpope/vim-fugitive"
+  },
+  ["vim-go"] = {
+    loaded = true,
+    path = "/Users/nic.young/.local/share/nvim/site/pack/packer/start/vim-go",
+    url = "https://github.com/fatih/vim-go"
+  },
+  ["vim-hardtime"] = {
+    loaded = true,
+    path = "/Users/nic.young/.local/share/nvim/site/pack/packer/start/vim-hardtime",
+    url = "https://github.com/takac/vim-hardtime"
+  },
+  ["vim-signify"] = {
+    loaded = true,
+    path = "/Users/nic.young/.local/share/nvim/site/pack/packer/start/vim-signify",
+    url = "https://github.com/mhinz/vim-signify"
+  },
+  ["vim-sneak"] = {
+    loaded = true,
+    path = "/Users/nic.young/.local/share/nvim/site/pack/packer/start/vim-sneak",
+    url = "https://github.com/justinmk/vim-sneak"
+  },
+  ["vim-surround"] = {
+    loaded = true,
+    path = "/Users/nic.young/.local/share/nvim/site/pack/packer/start/vim-surround",
+    url = "https://github.com/tpope/vim-surround"
+  },
+  vimwiki = {
+    loaded = true,
+    path = "/Users/nic.young/.local/share/nvim/site/pack/packer/start/vimwiki",
+    url = "https://github.com/vimwiki/vimwiki"
+  }
+}
+
+time([[Defining packer_plugins]], false)
+if should_profile then save_profiles() end
+
+end)
+
+if not no_errors then
+  vim.api.nvim_command('echohl ErrorMsg | echom "Error in packer_compiled: '..error_msg..'" | echom "Please check your config for correctness" | echohl None')
+end
