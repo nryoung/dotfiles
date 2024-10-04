74336d5ce31486a6d4f2703ad450cbccf3040a74
Nic Young <nryoung@gmail.com>
2024-10-03 22:34:48 -0600

    fix: (ubuntu) pasting in terminal

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,8 +1,15 @@
 local wezterm = require 'wezterm'
+local act = wezterm.action
 
 return {
   default_prog = { '/usr/bin/fish', '-l' },
   color_scheme = 'tokyonight_night',
   automatically_reload_config = true,
   font = wezterm.font 'FiraCode',
+  keys = {
+    -- paste from the clipboard
+    { key = 'v', mods = 'CTRL', action = act.PasteFrom 'Clipboard' },
+    -- paste from primary selection
+    { key = 'v', mods = 'CTRL', action = act.PasteFrom 'PrimarySelection' },
+  }
 }
