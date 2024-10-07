36c37a761019bd4634a409a27ac92318d65bfc29
Nic Young <nic@nicyoung.io>
2024-10-06 21:54:33 -0600

    feat: (ubuntu) modify and update init script with no-config tools

--- init.sh
+++ init.sh
@@ -14,53 +14,75 @@ symlink() {
     ln -s "${source}" "${target}"
 }
 
+print_status() {
+  # A function to display what step the script is on
+  echo "###############################################################################"
+  echo "$1"
+  echo "###############################################################################"
+}
+
 install_prereqs() {
-  # check to see if brew is installed. Install it if it isn't already
-  if ! which brew &> /dev/null; then
-    echo 'Installing `brew` package manager: https://brew.sh/'
-    echo 'Requires user password'
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  fi
 
-  # Install packages
-  echo "Installing packages with brew"
-  brew install \
-      neovim \
-      tmux \
-      fzf \
-      ripgrep \
-      ranger \
-      cmake \
-      gcc \
-      gh \
-      exa \
-      bat \
+  # Install pre-reqs
+  print_status "Installing pre-reqs such as cURL and wget. Requires password."
+  sudo apt install \
+    curl \
+    wget \
+
+  # Install rustup
+  print_status "Installing rustc toolchain. Requires input."
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+  # Install Rust specific tools
+  print_status "Installing Rust specific tools."
+  cargo install \
+      zellij \
+      eza \
       zoxide \
-      fish \
-      starship \
-      fd \
-      bat \
       git-delta \
-      shortcat
+      broot \
+      yazi-fm \
+      yazi-cli \
+  
+  # Install packages
+  print_status "Installing packages with apt."
+  # Add fish ppa
+  sudo apt-add-repository -y ppa:fish-shell/release-3
+  # Add wezterm repo
+	curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /usr/share/keyrings/wezterm-fury.gpg
+  echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
+  # add gh cli repo
+	sudo mkdir -p -m 755 /etc/apt/keyrings \
+	&& wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
 
-  brew tap wez/wezterm
-  brew install --cask wez/wezterm/wezterm
+  sudo apt update
+  sudo apt install \
+      ripgrep \
+      fzf \
+      bat \
+      fish \
+      fd-find \
+      wezterm \
+      gh \
 
-  # Install hammerspoon and ControlEscape spoon
-  mkdir -p ~/.hammerspoon/Spoons
-  git clone https://github.com/jasonrudolph/ControlEscape.spoon.git ~/.hammerspoon/Spoons/ControlEscape.spoon
-  cd ~/.hammerspoon/Spoons/ControlEscape.spoon
-  script/setup
+	# Install starship
+	print_status "Installing Starship."
+	curl -sS https://starship.rs/install.sh | sh
+
+	# Install lazygit
+	print_status "Installing Lazygit."
+	LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+  curl -Lo /tmp/lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+  tar xf /tmp/lazygit.tar.gz -C /tmp/
+  sudo install /tmp/lazygit /usr/local/bin
 }
 
 link_files() {
     local readonly dotfiles="${HOME}/dotfiles"
 
     # sync all the things
-    symlink "${dotfiles}/bash_aliases" ~/.bash_aliases
-    symlink "${dotfiles}/bash_profile" ~/.bash_profile
-    symlink "${dotfiles}/tmux.conf" ~/.tmux.conf
-    symlink "${dotfiles}/nvim" ~/.config/nvim
     symlink "${dotfiles}/gitconfig" ~/.gitconfig
     symlink "${dotfiles}/wezterm" ~/.config/wezterm
     symlink "${dotfiles}/config.fish" ~/.config/fish/config.fish
