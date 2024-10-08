5d90d4538f3857d675a60e15db97fa319b2137b3
Nic Young <nic@nicyoung.io>
2024-10-07 22:24:26 -0600

    feat: (all) add helix as default editor and add zellij aliases

--- config.fish
+++ config.fish
@@ -30,6 +30,37 @@ if status is-interactive
     # source lua-language-server
     if test -d ~/.lua-language-server
       export PATH="$HOME/.lua-language-server/bin:$PATH"
+
+    # Zellij alias
+    function zr
+        command zellij run --name "$argv" -- fish -c "$argv"
+    end
+    function zrf
+        command zellij run --name "$argv" --floating -- fish -c "$argv"
+    end
+    function zri
+        command zellij run --name "$argv" --in-place -- fish -c "$argv"
+    end
+    function ze
+        command zellij edit $argv
+    end
+    function zef
+        command zellij edit --floating $argv
+    end
+    function zei
+        command zellij edit --in-place $argv
+    end
+
+    # the zpipe alias and its completions
+    function __fish_complete_aliases
+        zellij list-aliases 2>/dev/null
+    end
+    function zpipe
+        if count $argv >/dev/null
+            command zellij pipe -p $argv
+        else
+            command zellij pipe
+        end
     end
 
     # work config (any work related config should go here and will not be checked in)
