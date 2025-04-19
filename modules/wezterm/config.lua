47f6b5dd1230e607628aa7e4f0d62ddbb855079c
Nic Young <nic@nicyoung.dev>
2025-04-18 19:44:02 -0600

    chore: update lock file and other small fixes

--- modules/wezterm/config.lua
+++ modules/wezterm/config.lua
@@ -3,10 +3,10 @@ local wezterm = require 'wezterm'
 return {
   color_scheme = 'Tokyo Night',
   automatically_reload_config = true,
-  enable_tab_bar = false,
+  -- enable_tab_bar = false,
   font = wezterm.font 'FiraCode',
   font_size = 10,
   -- to fix this issue: https://github.com/wez/wezterm/issues/5990
-  front_end = "WebGpu",
-  webgpu_power_preference = 'HighPerformance',
+  -- front_end = "WebGpu",
+  -- webgpu_power_preference = 'HighPerformance',
 }
