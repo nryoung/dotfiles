190afdcb00dd994a093ccca220b0d2898d1154ce
Nic Young <nic@nicyoung.dev>
2025-08-05 19:41:42 -0600

    fix: some things

--- modules/helix/default.nix
+++ modules/helix/default.nix
@@ -69,7 +69,7 @@
 
       keys = {
         normal = {
-          C-y = ":sh zellij run -f -x 10% -y 10% --width 80% --height 80% -- bash ~/.config/helix/yazi-picker.sh";
+          C-y = ":sh zellij run -n Yazi -c -f -x 10%% -y 10%% --width 80%% --height 80%% -- bash ~/.config/helix/yazi-picker.sh open %{buffer_name}";
           space = {
             F = "file_picker_in_current_buffer_directory";
           };
