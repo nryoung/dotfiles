79a25500478f57c75420b5733a6f8fae52528116
Nic Young <nic@nicyoung.io>
2025-08-14 09:17:54 -0600

    chore: fix some configuration issues

--- fish/config.fish
+++ fish/config.fish
@@ -104,8 +104,22 @@ end
 fish_add_path "/Users/nyoung/.local/bin"
 
 # pnpm
-set -gx PNPM_HOME "$HOME/Library/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-    set -gx PATH "$PNPM_HOME" $PATH
+# set -gx PNPM_HOME "$HOME/Library/pnpm"
+# if not string match -q -- $PNPM_HOME $PATH
+#     set -gx PATH "$PNPM_HOME" $PATH
+# end
+# # pnpm end
+
+# ASDF configuration code
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
 end
-# pnpm end
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
