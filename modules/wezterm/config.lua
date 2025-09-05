a49525f4b3e292c41b4f5ef20f6e1ee62c840fe6
Nic Young <nic@nicyoung.dev>
2025-09-05 14:33:33 -0600

    chore: change ordering

--- modules/wezterm/config.lua
+++ modules/wezterm/config.lua
@@ -5,5 +5,5 @@ return {
   automatically_reload_config = true,
   enable_tab_bar = false,
   enable_wayland= false,
-  font = wezterm.font_with_fallback { 'FiraCode NFM', 'FiraCode' },
+  font = wezterm.font_with_fallback { 'FiraCode', 'FiraCode NFM' },
 }
