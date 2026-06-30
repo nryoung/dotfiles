0b1fed49cb90c411534de303e4eceb7f4a765520
Nic Young <nic@nicyoung.dev>
2026-06-30 08:02:31 -0600

    chore: opencode changes

--- modules/opencode.nix
+++ modules/opencode.nix
@@ -4,6 +4,9 @@
     instructions = [ "~/.claude/skills/dev-workflow/SKILL.md" ];
     model = "ollama/hf.co/unsloth/gemma-4-12B-it-qat-GGUF:UD-Q4_K_XL";
     plugin = [ "context-mode" ];
+    compaction = {
+      auto = true;
+    };
     provider = {
       ollama = {
         npm = "@ai-sdk/openai-compatible";
@@ -12,10 +15,9 @@
           baseURL = "http://10.0.0.99:11434/v1";
         };
         models = {
-          "qwen2.5-coder:7b" = { name = "Qwen2.5 Coder 7B"; };
-          "qwen2.5-coder:14b" = { name = "Qwen2.5 Coder 14B"; };
-          "qwen3:8b" = { name = "Qwen3 8B"; };
-          "gemma4:12b" = { name = "Gemma 4 12B"; };
+          "hf.co/unsloth/Qwen3.5-9B-MTP-GGUF:UD-Q4_K_XL" = {
+            name = "Qwen3.5-9B-MTP-GGUF";
+          };
           "hf.co/unsloth/gemma-4-12B-it-qat-GGUF:UD-Q4_K_XL" = { name = "Gemma 4 12B QAT"; };
         };
       };
