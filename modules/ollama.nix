b3133f03ab5cdce9029ec6769fd74fbc189d7323
Nic Young <nic@nicyoung.dev>
2026-06-30 15:34:59 -0600

    chore: update ollama config to work a bit better

--- modules/ollama.nix
+++ modules/ollama.nix
@@ -5,8 +5,11 @@
     host = "0.0.0.0";
     port = 11434;
     environmentVariables = {
-      OLLAMA_CONTEXT_LENGTH = "32768";
+      OLLAMA_CONTEXT_LENGTH = "65536";
       OLLAMA_KV_CACHE_TYPE = "q8_0";
+      OLLAMA_FLASH_ATTENTION = "1";
+      OLLAMA_KEEP_ALIVE = "10m";
+      OLLAMA_NUM_PARALLEL = "1";
     };
   };
 }
